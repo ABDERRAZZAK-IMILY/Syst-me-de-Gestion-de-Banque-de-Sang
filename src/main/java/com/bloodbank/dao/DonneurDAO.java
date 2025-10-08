20d49f0e654c6aa234e2beb8e6220b7cb4f4b52f
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-10-08 17:29:28 +0200

    SGBS-11 : Create the DonneurDao

---
+++ src/main/java/com/bloodbank/dao/DonneurDAO.java
@@ -0,0 +1,23 @@
+package com.bloodbank.dao;
+
+import com.bloodbank.entity.Donneur;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class DonneurDAO {
+    private EntityManager em;
+
+    public DonneurDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(Donneur d) {
+        em.getTransaction().begin();
+        em.persist(d);
+        em.getTransaction().commit();
+    }
+
+    public List<Donneur> findAll() {
+        return em.createQuery("SELECT d FROM Donneur d", Donneur.class).getResultList();
+    }
+}
