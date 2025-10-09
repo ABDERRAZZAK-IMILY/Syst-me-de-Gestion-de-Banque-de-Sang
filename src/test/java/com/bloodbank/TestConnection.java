d753674a51663f2f678ae53473dea1804a1cb70a
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-10-09 12:54:08 +0200

    SGBS-19 Feat :Creat test Connection database

---
+++ src/test/java/com/bloodbank/TestConnection.java
@@ -0,0 +1,23 @@
+package com.bloodbank;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestConnection {
+    @Test
+    public void testEntityManagerConnection() {
+        try {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("bloodBankPU");
+            EntityManager em = emf.createEntityManager();
+            assertNotNull("EntityManager est null !", em);
+            em.close();
+            emf.close();
+        } catch (Exception e) {
+            fail("Erreur JPA : " + e.getMessage());
+        }
+    }
+}
